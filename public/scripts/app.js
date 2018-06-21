8c6888f552a70fe29601f00d416e3e215183d6d0
mindyli1120 <mindyli1989@gmail.com>
2018-06-21 00:07:14 +0000

    Add new tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,66 +5,19 @@
  */
 
 //database of user and tweet info:
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": {
-        "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-        "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-      },
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": {
-        "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-        "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-      },
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Johann von Goethe",
-      "avatars": {
-        "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-        "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-      },
-      "handle": "@johann49"
-    },
-    "content": {
-      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-    },
-    "created_at": 1461113796368
-  }
-];
-
 
 $(document).ready(function() {
   //function to render data to render new tweet;
   function renderTweets(tweets) {
-    for (const tweet in tweets) {
-      const data = tweets[tweet];
-      createTweetElement(data);
+    for (const tweet of tweets) {
+      let $data = createTweetElement(tweet);
+      $("#tweets-container").prepend($data);
     }
   }
 
   //function to create new tweet;
   function createTweetElement(newTweet) {
-    let $tweet = `
+    let tweet = `
     <article class="tweet">
       <header>
         <img class="avatar" src="${newTweet.user.avatars.small}">
@@ -79,9 +32,36 @@ $(document).ready(function() {
         <i class="fas fa-heart"></i></span>
       </footer>
     </article>`;
-    let $postTweet = $("#tweets-container").append($tweet);
-    return $postTweet;
+    // let $postTweet = $("#tweets-container").prepend($tweet);
+    return $(tweet);
   }
 
-  renderTweets(data);
+  $("form").on("submit", function(e) {
+    e.preventDefault();
+
+
+    let newData = $("form").serialize();
+    console.log(newData);
+
+  //3. ajex the data and append it to the page.
+    $.ajax("/tweets", {
+      method: "POST",
+      data: newData
+    }).done(function(data){
+        loadTweets();
+    });
+  })
+
+  //loading the tweets that has been composed.
+  function loadTweets() {
+    $.ajax("/tweets", {
+      method: "GET",
+    }).done(function(data){
+      $("#tweets-container").empty();
+      renderTweets(data);
+    })
+  };
+  loadTweets();
 });
+
+
